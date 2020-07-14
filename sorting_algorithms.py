1a5c23c421f21dfd999c1f2dc3de9073b5f0bc1c
Michael Steven <stevenpnd@gmail.com>
2020-07-14 00:12:51 -0500

    implementation of selection sort

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -18,3 +18,28 @@ def bubble_sort(numbers):
 x = [5, 1, 14, 9, 36, -6, 14, .2, 7, 0 , 7, 51, 2, 4]
 
 print(bubble_sort(x))
+
+def swap(arr, first, second):
+    arr[first], arr[second] = arr[second], arr[first]
+
+def get_minimum(arr, start):
+    minimum = arr[start]
+    index = start
+    for i in range(start + 1, len(arr)):
+        if arr[i] < minimum:
+            minimum = arr[i]
+            index = i
+    return index
+
+def selection_sort(arr):
+    new_arr = arr[:]
+    for i in range(0, len(new_arr) - 1):
+        index = get_minimum(new_arr, i + 1)
+        if new_arr[index] < new_arr[i]:
+            swap(new_arr, i, index)      
+    return new_arr
+
+print(selection_sort(x))
+
+
+
