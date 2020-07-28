6e591095490502d672ee201c33b4137a7c21261e
Michael Steven <stevenpnd@gmail.com>
2020-07-27 22:07:12 -0500

    implementation of merge sort

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -6,23 +6,22 @@ Created on Sun Jul 12 22:52:31 2020
 @author: michael
 """
 
+import math
+
 x = [5, 1, 14, 9, 36, -6, 14, .2, 7, 0 , 7, 51, 2, 4]
 
-def bubble_sort(numbers):
-    arr = numbers[:]
+def bubble_sort(arr):    
     n = len(arr)
     for i in range(0, n - 1):
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
-
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]    
     
 print("Bubble sort")
 print(x)
-print(bubble_sort(x))
-print()
-
+copy = x[:]
+bubble_sort(copy)
+print(copy)
 
 #-------------------------------------------------------
 def swap(arr, first, second):
@@ -37,18 +36,18 @@ def get_minimum(arr, start):
             index = i
     return index
 
-def selection_sort(arr):
-    new_arr = arr[:]
-    for i in range(0, len(new_arr) - 1):
-        index = get_minimum(new_arr, i + 1)
-        if new_arr[index] < new_arr[i]:
-            swap(new_arr, i, index)      
-    return new_arr
+def selection_sort(arr):    
+    for i in range(0, len(arr) - 1):
+        index = get_minimum(arr, i + 1)
+        if arr[index] < arr[i]:
+            swap(arr, i, index)    
 
 print("Selection sort")
 print(x)
-print(selection_sort(x))
-print()
+copy = x[:]
+selection_sort(copy)
+print(copy)
+
 #-------------------------------------------------------
 
 def insert(array, index, value):  
@@ -58,12 +57,68 @@ def insert(array, index, value):
         i -= 1
     array[i + 1] = value
 
-def insertion_sort(arr):
-    array = arr[:]
-    for i in range(0, len(array) - 1):
-        insert(array, i, array[i + 1])
-    return array
-
+def insertion_sort(arr):    
+    for i in range(0, len(arr) - 1):
+        insert(arr, i, arr[i + 1])    
+        
 print("Insertion sort")
 print(x)
-print(insertion_sort(x))
+copy = x[:]
+insertion_sort(copy)
+print(copy)
+
+#-------------------------------------------------------
+
+def merge(array, p, q, r):
+    
+    k = p
+    i = 0
+    j = 0
+    lowHalf = [None]*(q - k + 1)
+
+    while k <= q:
+        lowHalf[i] = array[k]
+        i += 1
+        k += 1
+
+    highHalf = [None]*(r - k + 1) 
+    while k <= r:
+        highHalf[j] = array[k]
+        j += 1
+        k += 1
+    
+    k = p
+    i = 0
+    j = 0
+
+    while i < len(lowHalf) and j < len(highHalf):
+        if lowHalf[i] <= highHalf[j]:
+            array[k] = lowHalf[i]
+            i += 1
+        else:
+            array[k] = highHalf[j]
+            j += 1
+        k += 1
+
+    while i < len(lowHalf):
+        array[k] = lowHalf[i]
+        i += 1
+        k += 1
+
+    while j < len(highHalf):
+        array[k] = highHalf[j]
+        j += 1
+        k += 1
+
+def merge_sort(array, p, r):
+    if p < r:
+        q = math.floor((p+r)/2)
+        merge_sort(array, p, q)
+        merge_sort(array, q + 1, r)
+        merge(array, p, q, r)
+
+print("Merge sort")
+print(x)
+copy = x[:]
+merge_sort(copy, 0, len(copy) - 1)
+print(copy)
